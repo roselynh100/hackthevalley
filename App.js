e6d82aa6d013ff1f1f79122a594ab47979d5b650
Christopher Leung <minecraft200times@gmail.com>
2022-10-15 12:34:11 -0400

    Navigation Bar Developed and Working. Need UI

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import NavBar from './src/navigation/navbar';
 
 export default function App() {
   return (
