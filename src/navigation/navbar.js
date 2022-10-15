78c713cb7a4722471ffc271d0720099849860895
Christopher Leung <minecraft200times@gmail.com>
2022-10-15 11:55:04 -0400

    Working on Nav and Setup of files

---
+++ src/navigation/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function NavBar() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
---
+++ src/screens/cookbook.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Cookbook() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Recipes Owned!</Text>
+      </View>
+    );
+  }
