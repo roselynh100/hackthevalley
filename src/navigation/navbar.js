822a3663bfcae3c1bd3bd0e437704eccf7f3a298
Christopher Leung <minecraft200times@gmail.com>
2022-10-16 07:15:25 -0400

    finished icons in navbar

--- src/navigation/navbar.js
+++ src/navigation/navbar.js
@@ -13,24 +13,31 @@ const Tab = createBottomTabNavigator();
 export default function NavBar() {
   return (
     <Tab.Navigator
-      // screenOptions={({route}) => {
-      //   tabBarIcon: ({focused, color, size}) => {
-      //     let iconName
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
 
-      //     if (route.name === 'Roll') {
-      //       iconName = focused
-      //         ? 'dice-multiple'
-      //         : 'dice-multiple-outline'
-      //     } else if (route.name === 'Cookbook') {
-      //       iconName = focused
-      //       ? 'book-open-page-variant'
-      //       : 'book-open-page-variant-outline'
-      //     }
-
-      //     return <MaterialCommunityIcons name={iconName} size={size} color={color} />
-      //   }
-      // }}>
-      >
+        if (route.name === 'Roll') {
+          iconName = focused ? "dice-multiple" : "dice-multiple-outline";
+          color = focused ? "#439ce0" : "gray";
+        } else if (route.name === 'Cookbook') {
+          iconName = focused ? "book-open" : "book-open-outline";
+          color = focused ? "#439ce0" : "gray";
+        } else if (route.name === 'Tasks') {
+          iconName = focused ? "sticker-check" : "sticker-check-outline";
+          color = focused ? "#439ce0" : "gray";
+        } else if (route.name === 'Shop') {
+          iconName = focused ? "shopping" : "shopping-outline";
+          color = focused ? "#439ce0" : "gray";
+        } else if (route.name === 'Inventory') {
+          iconName = focused ? "bag-personal" : "bag-personal-outline";
+          color = focused ? "#439ce0" : "gray";
+        }
+      
+        // You can return any component that you like here!
+        return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+      },
+    })}>
       <Tab.Screen name="Roll" component={Roll} />
       <Tab.Screen name="Cookbook" component={Cookbook} />
       <Tab.Screen name="Tasks" component={Tasks} />
