5b0554105ab29fc060b50ef504fc23d4df29cd43
Roselyn <roselynh100@gmail.com>
2022-10-16 00:37:40 -0400

    fixing weird nav stuff sob

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import React from 'react'
-import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native'
-import { Button } from '@rneui/themed'
-import TodoPage from './components/TodoPage'
-import Currency from './components/Currency'
-import Ingredient from './components/Ingredient'
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import NavBar from './src/navigation/navbar';
-
+import Currency from './src/components/Currency'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import NavBar from './src/navigation/navbar';
@@ -15,39 +7,10 @@ import NavBar from './src/navigation/navbar';
 const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <View style={styles.wrapper}>
-          <View style={styles.currency}>
-            <Currency style={styles.currency} />
-          </View>
-          {/* <TodoPage /> */}
-          <ScrollView style={styles.ingredients}>
-            <Ingredient name='Rice' image='https://i.pinimg.com/originals/b3/77/8e/b3778ed590bc573ecd266d6b7d92d658.png' owned='2' price='2' />
-            <Ingredient name='Rice' image='https://i.pinimg.com/originals/b3/77/8e/b3778ed590bc573ecd266d6b7d92d658.png' owned='2' price='2' />
-            <Ingredient name='Rice' image='https://i.pinimg.com/originals/b3/77/8e/b3778ed590bc573ecd266d6b7d92d658.png' owned='2' price='2' />
-            <Ingredient name='Rice' image='https://i.pinimg.com/originals/b3/77/8e/b3778ed590bc573ecd266d6b7d92d658.png' owned='2' price='2' />
-          </ScrollView>
-        </View>
+      <Currency />
       <NavBar />
-      </View>
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#AA8866'
-  },
-  wrapper: {
-    margin: 25
-  },
-  currency: {
-    alignItems: 'flex-end'
-  },
-  ingredients: {
-    marginBottom: 20
-  }
-})
 
 export default App
---
+++ src/components/Currency.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Chip } from '@rneui/themed'
+
+const Currency = () => {
+
+  const starNum = 2
+  const coinNum = 15
+
+  return (
+    <View style={styles.chipWrapper}>
+      <View style={styles.chipContainer}>
+        <Chip title={starNum + ' Stars'} style={styles.chip} />
+        <Chip title={coinNum + ' Coins'} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  chipWrapper: {
+    alignItems: 'flex-end',
+    paddingHorizontal: 20
+  },
+  chipContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 160,
+    paddingVertical: 20
+  }
+})
+
+export default Currency
